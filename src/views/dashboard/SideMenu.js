95d832b7ca88cc1694761125ede8a790215f0c5c
kyf <2842103033@qq.com>
2020-10-20 19:44:46 +0800

    test

--- src/views/dashboard/SideMenu.js
+++ src/views/dashboard/SideMenu.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Layout, Menu } from 'antd';
+import { withRouter } from 'react-router'
 //导入icon图标，使用哪个导入哪个
 import {
   UserOutlined,
@@ -8,36 +9,115 @@ import {
   EditOutlined
 } from '@ant-design/icons';
 
-const { Sider} = Layout;
+const { Sider } = Layout;
 const { SubMenu } = Menu;
 
+const menus = [
+  {
+    path: "/home",//为了点击某一个时，关联路由
+    //加单引号会当成字符串，直接写就行（jsx语法）
+    icon: <UserOutlined />,
+    title: "首页"
+  },
+  {
+    path: "/user-manage",
+    icon: <TeamOutlined />,
+    title: "用户管理",
+    children: [
+      {
+        path: "/user-manage/users",
+        icon: <TeamOutlined />,
+        title: "用户列表"
+      }
+    ]
+  },
+  {
+    path: "/right-manage",
+    icon: <UploadOutlined />,
+    title: "权限管理",
+    children: [
+      {
+        path: "/right-manage/rights",
+        icon: <UploadOutlined />,
+        title: "权限列表"
+      },
+      {
+        path: "/right-manage/roles",
+        icon: <UploadOutlined />,
+        title: "角色列表"
+      }
+    ]
+  },
+  {
+    path: "/article-manage",
+    icon: <EditOutlined />,
+    title: "文章管理",
+    children: [
+      {
+        path: "/article-manage/list",
+        icon: <EditOutlined />,
+        title: "文章列表"
+      },
+      {
+        path: "/article-manage/category",
+        icon: <EditOutlined />,
+        title: "文章内容"
+      }
+    ]
+  }
+]
 
-export default class SideMenu extends Component {
+class SideMenu extends Component {
   state = {
-    collapsed : false
+    collapsed: false
   }
   render() {
+    //以/为分隔符，分割成数组 /right-manage/rights" -> ["", "right-manage", "rights"]
+    let openKeys = ['/'+ this.props.location.pathname.split('/')[1]]
+    let selectKeys = [this.props.location.pathname]
     return (
-       <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
-        <div style={{color:"white",textAlign:"center"}}>内容管理系统</div>
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-          
-          <Menu.Item key="1" icon={<UserOutlined />}>
+      <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+        <div style={{ color: "white", textAlign: "center" }}>内容管理系统</div>
+        <Menu theme="dark" mode="inline" defaultOpenKeys={openKeys} selectedKeys={selectKeys}>
+          {/*使用这个defaultSelectedKeys的话，只能保证初始值,以后就非受控了;以后值改变也不会受影响;在menu源码中,只在cdm中接收
+          使用selectKeys属性，会让menu受控  */}
+          {/* 写死的话，不方便维护 */}
+          {/* <Menu.Item key="1" icon={<UserOutlined />}>
             首页
           </Menu.Item>
-          <Menu.Item key="2" icon={<TeamOutlined />}>
-            用户管理
-          </Menu.Item>
-          <SubMenu key="sub1" icon={<UploadOutlined />} title="权限管理">
-              <Menu.Item key="3">权限</Menu.Item>
-              <Menu.Item key="4">角色</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub2" icon={<EditOutlined />} title="文章管理">
-              <Menu.Item key="5">文章列表</Menu.Item>
-              <Menu.Item key="6">文章范围</Menu.Item>
-          </SubMenu>
-        </Menu> 
-      </Sider> 
+          <SubMenu key="sub1" icon={<TeamOutlined />} title="用户管理">
+              <Menu.Item key="2">用户列表</Menu.Item>
+          </SubMenu> */}
+          {
+            //写一个方法，动态渲染menu
+            this.renderMenu(menus)
+          }
+        </Menu>
+      </Sider>
     )
   }
+
+  renderMenu(menus) {
+    return menus.map(item => {
+      if (item.children) {
+        return <SubMenu key={item.path}
+          title={
+            <span>
+              {item.icon}
+              <span>{item.title}</span>
+            </span>
+          }>
+          {this.renderMenu(item.children)}
+        </SubMenu>
+      }
+      return <Menu.Item key={item.path} icon={item.icon} onClick={() => {
+        // console.log(item.path)
+        this.props.history.push(item.path) //编程式导航
+      }}>
+        {item.title}
+      </Menu.Item>
+    })
+  }
 }
+
+export default withRouter(SideMenu)
--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -1,11 +1,94 @@
 import React, { Component } from 'react'
-
+import Particles from 'react-particles-js';//引入粒子效果
+import { Form, Input, Button } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import './Login.css'
 export default class Login extends Component {
+  state = {
+    height: 0
+  }
   render() {
     return (
-      <div>
-        Login
+      <div style={{ backgroundColor: "skyblue" }}>
+        <Particles height={this.state.height}
+          params={
+            //配置信息
+            {
+              "particles": {
+                "number": {
+                  "value": 160,
+                  "density": {
+                    "enable": false
+                  }
+                },
+                "size": {
+                  "value": 10,
+                  "random": true
+                },
+                "move": {
+                  "direction": "bottom",
+                  "out_mode": "out"
+                },
+                "line_linked": {
+                  "enable": false
+                }
+              },
+              "interactivity": {
+                "events": {
+                  "onclick": {
+                    "enable": true,
+                    "mode": "remove"
+                  }
+                },
+                "modes": {
+                  "remove": {
+                    "particles_nb": 10
+                  }
+                }
+              }
+            }
+          } />
+        <div className="login-form">
+          <Form
+            name="normal_login"
+            initialValues={{ remember: true }}
+            onFinish={this.onFinish}
+          >
+            <Form.Item
+              name="username"
+              rules={[{ required: true, message: 'Please input your Username!' }]}
+            >
+              <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[{ required: true, message: 'Please input your Password!' }]}
+            >
+              <Input
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" className="login-form-button">
+                登录
+        </Button>
+            </Form.Item>
+          </Form>
+        </div>
       </div>
     )
+  }
+  componentDidMount() {
+    this.setState({
+      //粒子效果有bug，会多出来5px从而出现滚动条；减5px让滚动条消失
+      height: document.documentElement.clientHeight - 5 + 'px'
+    })
+  }
+  //提交表单且数据验证成功后回调事件
+  onFinish(values){
+    console.log(values)
+
   }
 }
